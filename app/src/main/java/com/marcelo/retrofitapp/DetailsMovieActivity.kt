61d9d614e7fd6e8aa4ca5ab62051556c6d1c09cc
Marcelo Moresco <marcelomoresco0@gmail.com>
2023-03-04 01:14:47 -0300

    Initial commit with implementation and the design files completed

---
+++ app/src/main/java/com/marcelo/retrofitapp/DetailsMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.marcelo.retrofitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_movie)
+    }
+}
